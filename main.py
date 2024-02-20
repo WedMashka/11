a6faf7a1f6c82454de7cb79670039282abbc3395
regul <regulusblack1991@gmail.com>
2024-02-20 23:42:02 +0300

    111

--- main.py
+++ main.py
@@ -10,5 +10,14 @@ ns = {'real_person': 'http://people.example.com',
 for actor in root.findall('real_person:actor', ns):
     name = actor.find('real_person:name', ns)
     print(name.text)
-    for char in actor.findall('role:character', ns):
-        print(' |-->', char.attrib)
+    for char in actor.findall('role:contract', ns):
+        if char.attrib.__contains__("trusted-ipilist"):
+            (char.attrib).pop("trusted-ipilist")
+            (char.attrib).pop("allow-trusted-ip-only")
+            # print(char.attrib)
+tree.write("test2.xml")
+            # char.pop("trusted-ipilist")
+            # char.pop("allow-trusted-ip-only")
+
+for elem in root.iter():
+    print(elem.tag, elem.attrib)
