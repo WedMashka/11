769da7f4d83d5b930c6d20ac95963299655bb9d7
regul <regulusblack1991@gmail.com>
2024-02-20 21:53:12 +0300

    11

---
+++ main.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+
+
+tree = ET.parse("test2.xml")
+root = tree.getroot()
+
+ns = {'real_person': 'http://people.example.com',
+      'role': 'http://characters.example.com'}
+
+for actor in root.findall('real_person:actor', ns):
+    name = actor.find('real_person:name', ns)
+    print(name.text)
+    for char in actor.findall('role:character', ns):
+        print(' |-->', char.attrib)
